1384e9eb21a4cb5f83b668eacc0d28b66a1f8bad
Eduardo leonardo <dudufox10@hotmail.com>
2019-02-04 11:31:30 -0200

    Criacao ItemPedidoRepository

--- AplicativoNetCore/Controllers/PedidoController.cs
+++ AplicativoNetCore/Controllers/PedidoController.cs
@@ -2,20 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AplicativoNetCore.repository;
+using AppEntity;
 using Microsoft.AspNetCore.Mvc;
+using Objetos;
 
 namespace AplicativoNetCore.Controllers
 {
     public class PedidoController : Controller
     {
-        public IActionResult Carrossel()
+
+        private readonly ApplicationContext context;
+        private readonly IItemPedidoRepository itemPedidoRepository;
+
+        public PedidoController(ApplicationContext context, IItemPedidoRepository itemPedidoRepository)
         {
-            return View();
+            this.context = context;
+            this.itemPedidoRepository = itemPedidoRepository;
         }
 
-        public IActionResult Carrinho()
+        public IActionResult Carrossel()
         {
-            return View();
+            var produtos = context.Set<Produto>().ToList();
+            return View(produtos);
+        }
+
+        public IActionResult Carrinho(string codigoProduto)
+        {
+            var pedido = itemPedidoRepository.AddItemPedido(codigoProduto);
+            return View(pedido);
         }
 
         public IActionResult Cadastro()
--- Objetos/ItemPedido.cs
+++ Objetos/ItemPedido.cs
@@ -6,6 +6,14 @@ namespace Objetos
 {
     public class ItemPedido
     {
+        public ItemPedido(Produto produto, decimal preco, int quantidade, Pedido pedido)
+        {
+            Produto = produto;
+            Preco = preco;
+            Quantidade = quantidade;
+            this.pedido = pedido;
+        }
+
         public int ItemPedidoId { get; set; }
         public Produto Produto { get; set; }
         public decimal Preco { get; set; }
